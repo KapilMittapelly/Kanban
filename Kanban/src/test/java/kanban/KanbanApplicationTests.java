eeca0ffca882177fa57921d453e0c827d6e05d98
Kapil <mittapellyk@gmail.com>
2017-10-27 00:16:13 -0500

    Testing eGit

--- Kanban/src/test/java/kanban/KanbanApplicationTests.java
+++ Kanban/src/test/java/kanban/KanbanApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class KanbanApplicationTests {
 
-	//Local Comment
+	//Local comment
 	@Test
 	public void contextLoads() {
 	}
